a09a69aa107bf1ae186d32f0cf33c07af2644486
Brian Carlstrom <bdc@google.com>
2010-06-30 00:57:36 -0700

    Add explict dependency on bouncycastle from CertificateInstaller
    Change-Id: I34b8edb51847f0e77c4d837210230667589a126c

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_JAVA_LIBRARIES := bouncycastle
 
 LOCAL_PACKAGE_NAME := CertInstaller
 LOCAL_CERTIFICATE := platform
