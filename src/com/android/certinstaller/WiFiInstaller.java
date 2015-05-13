122def31973a25d878f33ce59288792aea27a32d
Sanket Padawe <sanketpadawe@google.com>
2015-05-13 16:12:26 -0700

    Hotspot 2.0 credential UI code
    Bug: 20182930
    Change-Id: Iaac91833555f399acf8746ba11dd3007022205bd

--- src/com/android/certinstaller/WiFiInstaller.java
+++ src/com/android/certinstaller/WiFiInstaller.java
@@ -25,6 +25,11 @@ import java.util.Collection;
 public class WiFiInstaller extends Activity {
 
     private static final String TAG = "WifiInstaller";
+    private static final String NETWORK_NAME = "network_name";
+    private static final String INSTALL_STATE = "install_state";
+    WifiConfiguration mWifiConfiguration;
+    WifiManager mWifiManager;
+    boolean doNotInstall;
 
     @Override
     protected void onCreate(Bundle savedStates) {
@@ -38,208 +43,89 @@ public class WiFiInstaller extends Activity {
         Log.d(TAG, "WiFi data for " + CertInstallerMain.WIFI_CONFIG + ": " +
                 mimeType + " is " + (data != null ? data.length : "-"));
 
-        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        WifiConfiguration wifiConfiguration = wifiManager.buildWifiConfig(uriString, mimeType, data);
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        mWifiConfiguration = mWifiManager.buildWifiConfig(uriString, mimeType, data);
 
-        WifiEnterpriseConfig enterpriseConfig = wifiConfiguration.enterpriseConfig;
-        if (enterpriseConfig.getClientCertificate() != null ||
-                enterpriseConfig.getCaCertificate() != null) {
-            if (!KeyStore.getInstance().isUnlocked()) {
-                try {
-                    startActivity(new Intent(Credentials.UNLOCK_ACTION));
-                } catch (ActivityNotFoundException e) {
-                    Log.w(TAG, e);
+        if (mWifiConfiguration != null) {
+            WifiEnterpriseConfig enterpriseConfig = mWifiConfiguration.enterpriseConfig;
+            doNotInstall = (enterpriseConfig.getEapMethod() == WifiEnterpriseConfig.Eap.TTLS
+                    || enterpriseConfig.getEapMethod() == WifiEnterpriseConfig.Eap.TLS)
+                    && enterpriseConfig.getCaCertificate() == null;
+            if (!doNotInstall && (enterpriseConfig.getClientCertificate() != null
+                    || enterpriseConfig.getCaCertificate() != null)) {
+                if (!KeyStore.getInstance().isUnlocked()) {
+                    try {
+                        startActivity(new Intent(Credentials.UNLOCK_ACTION));
+                    } catch (ActivityNotFoundException e) {
+                        Log.w(TAG, e);
+                    }
                 }
             }
-        }
-        createMainDialog(wifiManager, wifiConfiguration);
-    }
-
-    private static String roamingConsortiumsToString(Collection<Long> ois) {
-        StringBuilder sb = new StringBuilder();
-        boolean first = true;
-        for (long oi : ois) {
-            if (first) {
-                first = false;
-            } else {
-                sb.append(", ");
-            }
-            if (Long.numberOfLeadingZeros(oi) > 40) {
-                sb.append(String.format("%06x", oi));
-            } else {
-                sb.append(String.format("%010x", oi));
-            }
-        }
-        return sb.toString();
-    }
-
-    private static String mapEAPMethod(WifiEnterpriseConfig config) {
-        switch (config.getEapMethod()) {
-            case WifiEnterpriseConfig.Eap.TTLS:
-                return "TTLS+" + mapInnerMethod(config.getPhase2Method());
-            case WifiEnterpriseConfig.Eap.TLS:
-                return "TLS";
-            case WifiEnterpriseConfig.Eap.SIM:
-                return "SIM";
-            case WifiEnterpriseConfig.Eap.AKA:
-                return "AKA";
-            case WifiEnterpriseConfig.Eap.AKA_PRIME:
-                return "AKA'";
-            default:
-                return String.format("Unsupported method %d", config.getEapMethod());
+        } else {
+            doNotInstall = true;
         }
     }
 
-    private static String mapInnerMethod(int id) {
-        switch (id) {
-            case WifiEnterpriseConfig.Phase2.NONE:
-                return "none";
-            case WifiEnterpriseConfig.Phase2.PAP:
-                return "PAP";
-            case WifiEnterpriseConfig.Phase2.MSCHAP:
-                return "MS-CHAP";
-            case WifiEnterpriseConfig.Phase2.MSCHAPV2:
-                return "MS-CHAPv2";
-            case WifiEnterpriseConfig.Phase2.GTC:
-                return "GTC";
-            default:
-                return String.format("Unsupported inner method %d", id);
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        createMainDialog();
     }
 
-    private void createMainDialog(final WifiManager wifiManager, final WifiConfiguration config) {
+    private void createMainDialog() {
         Resources res = getResources();
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         View layout = getLayoutInflater().inflate(R.layout.wifi_main_dialog, null);
         builder.setView(layout);
 
         TextView text = (TextView) layout.findViewById(R.id.wifi_info);
-        text.setText(String.format("WiFi configuration from %s (%s)",
-                config.providerFriendlyName, config.FQDN));
+        if (!doNotInstall) {
+            text.setText(String.format(getResources().getString(R.string.wifi_installer_detail),
+                    mWifiConfiguration.providerFriendlyName));
 
-        Button detailButton = (Button) layout.findViewById(R.id.wifi_detail);
+            builder.setTitle(mWifiConfiguration.providerFriendlyName);
+            builder.setIcon(res.getDrawable(R.drawable.signal_wifi_4_bar_lock_black_24dp));
 
-        detailButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createDetailDialog(config);
-            }
-        });
+            builder.setPositiveButton(R.string.wifi_install_label,
+                    new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if(mWifiManager.addNetwork(mWifiConfiguration) != -1
+                            && mWifiManager.saveConfiguration()) {
+                        Intent intent = new Intent(getApplicationContext(),
+                                CredentialsInstallDialog.class);
+                        intent.putExtra(NETWORK_NAME, mWifiConfiguration.providerFriendlyName);
+                        intent.putExtra(INSTALL_STATE, 1);
+                        startActivity(intent);
+                    } else {
+                        Intent intent = new Intent(getApplicationContext(),
+                                CredentialsInstallDialog.class);
+                        intent.putExtra(INSTALL_STATE, 0);
+                        startActivity(intent);
+                    }
+                    dialog.dismiss();
+                    finish();
+                }
+            });
 
-        builder.setTitle(res.getString(R.string.wifi_title));
-
-        builder.setPositiveButton(R.string.wifi_install_label, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Log.d("WFDL", "OK");
-                wifiManager.addNetwork(config);
-                wifiManager.saveConfiguration();
-                dialog.dismiss();
-                finish();
-            }
-        });
-
-        builder.setNegativeButton(R.string.wifi_cancel_label, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Log.d("WFDL", "Cancel");
-                dialog.dismiss();
-                finish();
-            }
-        });
-
-        builder.create().show();
-    }
-
-    private void createDetailDialog(WifiConfiguration config) {
-        Resources res = getResources();
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        View layout = getLayoutInflater().inflate(R.layout.wifi_detail_dialog, null);
-        layout.setHorizontalScrollBarEnabled(true);
-        builder.setView(layout);
-
-        TextView text = (TextView) layout.findViewById(R.id.wifi_detail_text);
-        text.setText(getDetailedInfo(res, config));
-
-        builder.setTitle(String.format(res.getString(R.string.wifi_detail_title),
-                config.providerFriendlyName));
-
-        builder.setPositiveButton(R.string.wifi_dismiss_label,
-                new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Log.d("WFDL", "detail dismiss");
-                dialog.dismiss();
-            }
-        });
-
-        builder.create().show();
-    }
-
-    private static String getDetailedInfo(Resources res, WifiConfiguration wifiConfiguration) {
-        if (wifiConfiguration == null) {
-            return res.getString(R.string.wifi_no_config);
+            builder.setNegativeButton(R.string.wifi_cancel_label, new
+                    DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    finish();
+                }
+            });
+        } else {
+            text.setText(getResources().getString(R.string.wifi_installer_download_error));
+            builder.setPositiveButton(R.string.done_label, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    finish();
+                }
+            });
         }
-
-        StringBuilder details = new StringBuilder();
-        WifiEnterpriseConfig enterpriseConfig = wifiConfiguration.enterpriseConfig;
-        details.append(String.format(res.getString(R.string.wifi_config_text),
-                wifiConfiguration.providerFriendlyName,
-                wifiConfiguration.FQDN,
-                roamingConsortiumsToString(wifiConfiguration.roamingConsortiumIds),
-                enterpriseConfig.getRealm(),
-                mapEAPMethod(enterpriseConfig)));
-
-        switch (enterpriseConfig.getEapMethod()) {
-            case WifiEnterpriseConfig.Eap.TTLS:
-                details.append(String.format(res.getString(R.string.wifi_ttls_config_text), enterpriseConfig.getIdentity()));
-                details.append("Password: ").append(enterpriseConfig.getPassword()).append('\n');   // !!!
-                if (enterpriseConfig.getCaCertificate() != null) {
-                    details.append(String.format(res.getString(R.string.wifi_trust_config_text), enterpriseConfig.getCaCertificate()));
-                }
-                break;
-            case WifiEnterpriseConfig.Eap.TLS:
-                PrivateKey key = enterpriseConfig.getClientPrivateKey();
-                String keyInfo;
-                if (key instanceof RSAPrivateKey) {
-                    RSAPrivateKey rsaKey = (RSAPrivateKey) key;
-                    int bits = rsaKey.getModulus().bitLength();
-                    keyInfo = "RSA " + (((bits + 7) / 8)*8) + " bits";
-                }
-                else {
-                    keyInfo = key.getAlgorithm();
-                }
-                details.append(String.format(res.getString(R.string.wifi_tls_config_text), enterpriseConfig.getClientCertificate(), keyInfo));
-                if (enterpriseConfig.getCaCertificate() != null) {
-                    details.append(String.format(res.getString(R.string.wifi_trust_config_text), enterpriseConfig.getCaCertificate()));
-                }
-                break;
-            case WifiEnterpriseConfig.Eap.SIM:
-            case WifiEnterpriseConfig.Eap.AKA:
-            case WifiEnterpriseConfig.Eap.AKA_PRIME:
-                details.append(String.format(res.getString(R.string.wifi_sim_config_text), enterpriseConfig.getPlmn()));
-                break;
-        }
-        return details.toString();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outStates) {
-        super.onSaveInstanceState(outStates);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        finish();
+        builder.create().show();
     }
 }
